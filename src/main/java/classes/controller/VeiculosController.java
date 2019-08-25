0baf1c2c4c08d97b5cb30ea7198ef52b579d030c
Werik Pinheiro <werikpds@gmail.com>
2019-08-25 14:19:08 -0300

    testando

--- src/main/java/classes/controller/VeiculosController.java
+++ src/main/java/classes/controller/VeiculosController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VeiculosController {
 	
-	@GetMapping("/lista")
+	@GetMapping("/hello")
 	public String hello() {
 		return "lista_veiculos";
 	}
