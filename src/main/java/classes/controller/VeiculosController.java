62cce4b5620fa2e78b40e2e285dfbd15398c542a
Werik Pinheiro <werikpds@gmail.com>
2019-08-27 22:09:48 -0300

    finalparte1

--- src/main/java/classes/controller/VeiculosController.java
+++ src/main/java/classes/controller/VeiculosController.java
@@ -1,5 +1,6 @@
 package classes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +10,8 @@ import classes.repository.VeiculosRepository;
 
 @Controller
 public class VeiculosController {
+	
+	@Autowired
 	VeiculosRepository veiculosRepo;
 	
 	@GetMapping("/lista")
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into veiculo (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'AZUL', 2019);
-insert into veiculo (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'MARRON', 2019);
-insert into veiculo (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'AMARELO', 2019);
+insert into veiculo (id, modelo, marca, cor, ano_Fabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'AZUL', 2019);
+insert into veiculo (id, modelo, marca, cor, ano_Fabricacao) values (2, 'BOL', 'VOLKSWAGEN', 'MARRON', 2019);
+insert into veiculo (id, modelo, marca, cor, ano_Fabricacao) values (3, 'BOL', 'VOLKSWAGEN', 'AMARELO', 2019);
