16d35d45d3427f2405a2384f45f8b95034e25bdc
Werik Pinheiro <werikpds@gmail.com>
2019-08-27 21:57:11 -0300

    import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into aluno (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'AZUL', 2019);
-insert into aluno (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'MARRON', 2019);
-insert into aluno (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'AMARELO', 2019);
+insert into veiculo (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'AZUL', 2019);
+insert into veiculo (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'MARRON', 2019);
+insert into veiculo (id, modelo, marca, cor, anoFabricacao) values (1, 'BOL', 'VOLKSWAGEN', 'AMARELO', 2019);
