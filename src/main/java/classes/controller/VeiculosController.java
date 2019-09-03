afd55a32e700f0f1cb8c2d5961fddcffc7625d3e
Werik Pinheiro <werikpds@gmail.com>
2019-09-02 22:45:28 -0300

    Inclusão de carros

--- src/main/java/classes/controller/VeiculosController.java
+++ src/main/java/classes/controller/VeiculosController.java
@@ -3,6 +3,7 @@ package classes.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import classes.model.Veiculo;
@@ -19,6 +20,19 @@ public class VeiculosController {
 		ModelAndView mv = new ModelAndView("lista_veiculos");
 		java.util.List<Veiculo> veiculos = veiculosRepo.findAll();
 		mv.addObject("veiculos", veiculos);
+		mv.addObject(new Veiculo());
 		return mv;
 	}
+	
+	@PostMapping("/lista")
+	public String salvar (Veiculo veiculo) {
+		veiculo.setId((long) 0);
+		java.util.List<Veiculo> veiculos = veiculosRepo.findAll();
+		for(int i = 0; i < veiculos.size(); i++) {
+			if(veiculos.get(i).getId() > veiculo.getId()) veiculo.setId(veiculos.get(i).getId());
+			else veiculo.setId(veiculo.getId()+1);
+		}
+		veiculosRepo.save(veiculo);
+		return "redirect:/lista";
+	} 
 }
